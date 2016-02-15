3ff81976f2eab87c7c10011c2b40d148baaa9774
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-15 12:29:24 -0800

    Allow user to enter new items into fridgeStorage array

--- client/app/fridge/fridge.js
+++ client/app/fridge/fridge.js
@@ -3,6 +3,8 @@ angular.module('fridgeKeep.fridge', [])
 .controller('FridgeController', function ($scope) {
   $scope.item = {};
   $scope.addItem = function () {
-
+    fridgeStorage.push(JSON.parse(JSON.stringify($scope.item)));
+    $scope.item.name = '';
+    $scope.item.expDate = '';
   };
 });
