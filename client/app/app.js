8a74fe2b31aaf21f016b06e87a8f45a641208f6e
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-15 15:55:08 -0800

    Add infrastructure for authentication

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,46 @@
 angular.module('fridgeKeep', [
   'ngRoute',
-  'fridgeKeep.fridge'
+  'fridgeKeep.fridge',
+  'fridgeKeep.auth'
 ])
-.config(function ($routeProvider) {
+.config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'app/fridge/fridge.html',
       controller: 'FridgeController'
     })
+    .when('/signin', {
+      templateUrl: 'app/auth/signin.html'
+      controller: 'AuthController'
+    })
+    .when('/signin', {
+      templateUrl: 'app/auth/signin.html'
+      controller: 'AuthController'
+    })
     .otherwise({
       redirectTo: '/' 
     });
+    $httpProvider.interceptors.push('AttachTokens');
 })
+.factory('AttachTokens', function ($window) {
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.shortly');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function ($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
 
 var fridgeStorage = [];
--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var db = require('./config/db');
+var userController = require('./users/userController.js');
+var fridgeController = require('./users/userController.js');
+var jwt = require('jwt-simple');
 
 var app = express();
-
 
 var port = process.env.PORT || 8080; 
 
@@ -13,6 +15,27 @@ mongoose.connect(db.url);
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
+app.post('/api/users/signin', userController.signin);
+app.post('/api/users/signup', userController.signup);
+app.get('/api/users/signedin', userController.checkAuth);
+
+app.use('/api/fridge', function (req, res, next) {
+    var token = req.headers['x-access-token'];
+    var user;
+    if (!token) {
+      return res.send(403); // send forbidden if a token is not provided
+    }
+    try {
+      user = jwt.decode(token, 'secret');
+      req.user = user;
+      next();
+    } catch (error) {
+      return next(error);
+    }
+});
+// app.get('/api/fridge/', userController.getFridgeItems);
+// app.get('/api/stats/', userController.getStats);
+// app.post('/api/fridge/', userController.addNewItem);
 
 // configure our server with all the middleware and routing
 // require('./config/routes.js')(app, express);
