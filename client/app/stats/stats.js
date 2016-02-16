75760dce6597e6984cc7c057243edd728c2dd173
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-16 15:06:06 -0800

    Add and format line chart

--- client/app/stats/stats.js
+++ client/app/stats/stats.js
@@ -1,6 +1,7 @@
 angular.module('fridgeKeep.stats', ['fridgeKeep.services'])
 
 .controller('StatsController', function ($scope, Auth, UserActions) {
+  $scope.signout = Auth.signout;
   $scope.daysToExpiry = function (expDate) {
     return Math.floor((new Date(expDate) - new Date()) / 1000 / 60 / 60 / 24);
   };
@@ -30,22 +31,52 @@ angular.module('fridgeKeep.stats', ['fridgeKeep.services'])
       }, 0)
       $scope.consumedQty = $scope.trashedQty + $scope.eatenQty;
       $scope.pieChart();
+      $scope.lineChart();
     });
   }
   $scope.pieChart = function() {
-    console.log($scope.eatenQty, $scope.consumedQty, 100);
     new Morris.Donut({
-      // ID of the element in which to draw the chart.
       element: 'pieChart',
-      // Chart data records -- each entry in this array corresponds to a point on
-      // the chart.
       data: [
         { label: 'Eaten', value:$scope.eatenQty / $scope.consumedQty * 100},
         { label: 'Trashed', value:$scope.trashedQty / $scope.consumedQty * 100 }
       ],
       formatter: function (y, data) { return y.toFixed(1) + '%' } 
     });
-  }
+  };
+  $scope.lineChart = function() {
+    var trashMonths = {};
+    var stomachMonths = {};
+    for(var i = 0; i < $scope.trash.length; i++) {
+      var date = new Date($scope.trash[i].dateFinished);
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var monthyear = '' + month + year;
+      trashMonths[monthyear] ? trashMonths[monthyear] += $scope.trash[i].value : trashMonths[monthyear] = $scope.trash[i].value;
+    }
+    for(var i = 0; i < $scope.stomach.length; i++) {
+      var date = new Date($scope.stomach[i].dateFinished);
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var monthyear = '' + month + year;
+      stomachMonths[monthyear] ? stomachMonths[monthyear] += $scope.stomach[i].value : stomachMonths[monthyear] = $scope.stomach[i].value;
+    }
+    var data = [];
+    for(key in trashMonths) {
+      var keyArr = key.split('');
+      var year = keyArr.splice(-4,4).join('');
+      var entry = {month:(Number(keyArr[0]) + 1) + '/' + year, value:trashMonths[key]};
+      data.push(entry);
+    }
+    new Morris.Line({
+      element: 'lineChart',
+      data: data,
+      xkey: 'month',
+      ykeys: ['value'],
+      labels: ['Wasted food'],
+      yLabelFormat:function (y) { return '$' + y.toFixed(2); }
+    });
+  };
   $scope.getInfo();
  });
 
