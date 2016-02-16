6b5d6d3532f233a7e26a92f6e5dac7f5bbbd0ea8
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-16 12:26:25 -0800

    Add pie chart to stats page

--- client/app/stats/stats.js
+++ client/app/stats/stats.js
@@ -29,7 +29,25 @@ angular.module('fridgeKeep.stats', ['fridgeKeep.services'])
         return accumulator;
       }, 0)
       $scope.consumedQty = $scope.trashedQty + $scope.eatenQty;
+      $scope.pieChart();
+    });
+  }
+  $scope.pieChart = function() {
+    console.log($scope.eatenQty, $scope.consumedQty, 100);
+    new Morris.Donut({
+      // ID of the element in which to draw the chart.
+      element: 'pieChart',
+      // Chart data records -- each entry in this array corresponds to a point on
+      // the chart.
+      data: [
+        { label: 'Eaten', value:$scope.eatenQty / $scope.consumedQty * 100},
+        { label: 'Trashed', value:$scope.trashedQty / $scope.consumedQty * 100 }
+      ],
+      formatter: function (y, data) { return y.toFixed(1) + '%' } 
     });
   }
   $scope.getInfo();
  });
+
+
+
